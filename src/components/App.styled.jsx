cb6aee12cd51f737e7311db38e936121f42845f6
Roman-Nechaev <nrchaevroman@gmail.com>
2023-03-24 13:27:31 +0200

    add margin Container

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  margin: 0 auto;
+  margin: 50px auto;
+
   padding: 0 15px;
   width: 400px;
   border: 1px solid rgb(189, 189, 189);
